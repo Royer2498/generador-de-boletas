05a79734359ab4ca90dcb9f96a26a88ff8599f23
Royer2498 <royer_2498@hotmail.com>
2019-03-23 10:01:42 -0400

    test para verificar la fecha de la factura

--- GeneradorBoleta.js
+++ GeneradorBoleta.js
@@ -1,16 +1,19 @@
+export class GeneradorBoleta {
+  constructor(empleado) {
+    this.empleado = empleado;
+  }
 
-export class GeneradorBoleta{
+  get monto() {
+    return this.empleado.sueldo;
+  }
 
-    constructor(empleado){
-        this.empleado = empleado;
-    }
+  get contribuyente() {
+    return this.empleado.nombre;
+  }
 
-    get monto(){
-        return this.empleado.sueldo;
-    }
-
-    get contribuyente(){
-        return this.empleado.nombre;
-    }
-
+  get fecha() {
+    var fechaActual = String(new Date());
+    var fechaConFormato = fechaActual.slice(0, 15);
+    return fechaConFormato;
+  }
 }
--- test/GeneradorBoleta_test.js
+++ test/GeneradorBoleta_test.js
@@ -5,27 +5,23 @@ var should = require("chai").should();
 import { GeneradorBoleta } from "../GeneradorBoleta";
 import { Empleado } from "../Empleado.js";
 
-
 describe("BoletaDePagoTest", function() {
-  
   it("si se genera una boleta con monto 100 para un empleado con sueldo fijo el monto de la boleta deberia ser 100", function() {
-    let empleado = new Empleado("JUAN PEREZ",100);  
+    let empleado = new Empleado("JUAN PEREZ", 100);
     let boletaPago = new GeneradorBoleta(empleado);
     expect(boletaPago.monto).equal(100);
   });
 
   it("si se genera una boleta para Juan Perez el contribuyente de la boleta deberia ser Juan Perez", function() {
-    let empleado = new Empleado("JUAN PEREZ",100);  
+    let empleado = new Empleado("JUAN PEREZ", 100);
     let boletaPago = new GeneradorBoleta(empleado);
     expect(boletaPago.contribuyente).equal("JUAN PEREZ");
   });
 
-  
-
-
-
-
-
-
-  
+  it("si se genera una boleta en una fecha especifica la fecha de la boleta deberia ser la misma", function() {
+    let empleado = new Empleado("JUAN PEREZ", 100);
+    let boletaPago = new GeneradorBoleta(empleado);
+    let fechaActualConFormato = String(new Date()).slice(0, 15);
+    expect(boletaPago.fecha).equal(fechaActualConFormato);
+  });
 });
