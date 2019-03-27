a44e5ca8e4c85c8ac00463afaf76a003518f6d33
Ronald Escobar <ronaldescobarj@gmail.com>
2019-03-27 17:47:54 -0400

    test pasando de empleado de tiempo completo

--- EmpleadoTIempoCompleto.js
+++ EmpleadoTIempoCompleto.js
@@ -1,11 +1,13 @@
 import { Empleado } from "./Empleado";
+import { CalculadoraTiempoCompleto } from "./CalculadoraTiempoCompleto";
 
 export class EmpleadoTiempoCompleto extends Empleado {
     constructor(nombre, salario, cargo) {
         super(nombre, salario, cargo);
+        this.calculadora = new CalculadoraTiempoCompleto();
     }
 
     calcularSalario() {
-        return this.salario.monto;
+        return this.calculadora.calcularSalario(this);
     }
 }
--- test/Empleados_test.js
+++ test/Empleados_test.js
@@ -2,8 +2,15 @@ var expect = require("chai").expect;
 
 import { Salario } from "../Salario.js";
 import { EmpleadoPorComision } from "../EmpleadoPorComision";
+import { EmpleadoTiempoCompleto } from "../EmpleadoTiempoCompleto";
 
 describe("Empleados", function () {
+
+    it(`el sueldo para un empleado fijo con salario 7000 deberia ser 7000`, function () {
+        let salario = new Salario(7000, "Bs")
+        let empleado = new EmpleadoTiempoCompleto("Juan Perez", salario, "Gerente");
+        expect(empleado.calcularSalario()).equal(7000);
+    });
 
     it(`el sueldo para un empleado por comision con sueldo base 100, 1000bs vendidos
     y 10% de comision deberia ser 200`, function () {
