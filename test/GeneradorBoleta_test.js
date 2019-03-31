16b23ed24e0f48fe1c1c46f6d87d9ac294176b84
Ronald Escobar <ronaldescobarj@gmail.com>
2019-03-31 17:52:39 -0400

    test pasando de la boleta de pago para un empleado por comision

--- test/GeneradorBoleta_test.js
+++ test/GeneradorBoleta_test.js
@@ -36,6 +36,18 @@ describe("BoletaDePagoTest", function () {
   it(`si se genera una boleta para Fernando Gonzalez que recibe 100 bolivianos de monto base, un 10% de
   comision y 1000 bs en ventas, es comisionado de ventas y la moneda es Bs, la boleta deberia proveer
   toda la informacion`, function () {
+      let empleado = new Empleado("Juan Perez", 123, "Por comision", "Gerente");
+      empleado.establecerSueldoBase(100);
+      empleado.establecerPorcentajeDeComision(10);
+      empleado.aniadirMontoVendido(1000);
+      let boletaPago = new GeneradorBoleta(empleado, "Cochabamba");
+      let fechaActualConFormato = String(new Date()).slice(0, 15);
+      let boletaImpresa = `BOLETA DE PAGO
+    Empleado: Juan Perez
+    Cargo: Gerente
+    Salario: 200 Bolivianos
+    Fecha de emision: ${fechaActualConFormato}`;
+      expect(boletaPago.imprimir()).eq(boletaImpresa);
     });
 
 
