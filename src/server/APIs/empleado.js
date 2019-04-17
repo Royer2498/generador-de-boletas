f6ec821fd2267afe3a51353f1f9d868c98bb597e
Ronald Escobar <ronaldescobarj@gmail.com>
2019-04-17 18:59:34 -0400

    refactorizado las apis de empleado

--- src/server/APIs/empleado.js
+++ src/server/APIs/empleado.js
@@ -7,79 +7,86 @@ setTimeout(function () {
     coleccionEmpleados = conexionABaseDeDatos.baseDeDatos.collection('empleados');
 }, 2000);
 
-router.get("/obtener-todos", function (req, res) {
-    conexionABaseDeDatos.obtenerTodos(coleccionEmpleados, function (err, results) {
-        if (err)
-            res.send(418).send(err.stack);
+function manejarError(respuesta, mensajeDeError, codigo) {
+    respuesta.send(codigo).send(mensajeDeError);
+}
+
+function hayResultados(resultados) {
+    return resultados.length > 0
+}
+
+router.get("/obtener-todos", function (consulta, respuesta) {
+    conexionABaseDeDatos.obtenerTodos(coleccionEmpleados, function (error, resultados) {
+        if (error)
+            manejarError(respuesta, error.stack, 418);
         else
-            res.send(results);
+            respuesta.send(resultados);
     })
 })
 
-router.get("/:ci", function (req, res) {
-    let criterioDeBusqueda = { ci: parseInt(req.params.ci) };
-    conexionABaseDeDatos.buscar(criterioDeBusqueda, coleccionEmpleados, function (err, results) {
-        if (err)
-            res.status(409).send(err.stack);
-        else if (results.length == 0)
-            res.status(404).send("no se encontro el empleado")
+router.get("/:ci", function (consulta, respuesta) {
+    let criterioDeBusqueda = { ci: parseInt(consulta.params.ci) };
+    conexionABaseDeDatos.buscar(criterioDeBusqueda, coleccionEmpleados, function (error, resultados) {
+        if (error)
+            manejarError(respuesta, error.stack, 409);
+        else if (!hayResultados())
+            manejarError(respuesta, "No se encontro al empleado", 404)
         else
-            res.send(results);
+            respuesta.send(resultados);
     })
 })
 
-router.post("/", function (req, res) {
-    let empleado = req.body;
+router.post("/", function (consulta, respuesta) {
+    let empleado = consulta.body;
     let criterioDeBusqueda = { ci: parseInt(empleado.ci) }
-    conexionABaseDeDatos.buscar(criterioDeBusqueda, coleccionEmpleados, function (err, results) {
-        if (err)
-            res.status(409).send(err.stack);
-        else if (results.length > 0)
-            res.status(409).send("ya existe un empleado con ese ci");
+    conexionABaseDeDatos.buscar(criterioDeBusqueda, coleccionEmpleados, function (error, resultados) {
+        if (error)
+            manejarError(respuesta, error.stack, 409)
+        else if (hayResultados())
+            manejarError(respuesta, "Ya existe un empleado con ese CI", 409)
         else {
-            conexionABaseDeDatos.insertar(empleado, coleccionEmpleados, function (err, resp) {
-                if (err)
-                    res.status(409).send(err.stack);
-                res.send("insertado exitosamente");
+            conexionABaseDeDatos.insertar(empleado, coleccionEmpleados, function (error, resp) {
+                if (error)
+                    manejarError(respuesta, error.stack, 409)
+                respuesta.send("insertado exitosamente");
             })
         }
 
     })
 })
 
-router.put("/:ci", function (req, res) {
-    let empleado = req.body;
-    empleado.ci = parseInt(empleado.ci);
-    let criterioDeBusqueda = { ci: parseInt(req.params.ci) }
-
-    conexionABaseDeDatos.buscar(criterioDeBusqueda, coleccionEmpleados, function (err, results) {
-        if (err)
-            res.status(409).send(err.stack);
-        else if (results.length == 0)
-            res.status(404).send("no se encontro el empleado");
+router.put("/", function (consulta, respuesta) {
+    let empleado = consulta.body;
+    let criterioDeBusqueda = { ci: parseInt(empleado.ci) }
+    conexionABaseDeDatos.buscar(criterioDeBusqueda, coleccionEmpleados, function (error, resultados) {
+        if (error)
+            manejarError(respuesta, error.stack, 409)
+        else if (!hayResultados(resultados))
+            manejarError(respuesta, "No se encontro al empleado", 404)
         else {
-            conexionABaseDeDatos.actualizar({ ci: empleado.ci }, { $set: empleado }, coleccionEmpleados,
-                function (err, resp) {
-                    if (err)
-                        res.status(409).send(err.stack);
-                    res.send("actualizado exitosamente");
+            let empleadoActualizado = { $set: empleado }
+            conexionABaseDeDatos.actualizar(criterioDeBusqueda, empleadoActualizado, coleccionEmpleados,
+                function (error, resp) {
+                    if (error)
+                        manejarError(respuesta, error.stack, 409)
+                    respuesta.send("actualizado exitosamente");
                 })
         }
     })
 })
 
-router.delete("/:ci", function (req, res) {
-    let criterioDeBusqueda = { ci: parseInt(req.params.ci) };
-    conexionABaseDeDatos.buscar(criterioDeBusqueda, coleccionEmpleados, function (err, results) {
-        if (err)
-            res.status(409).send(err.stack);
-        else if (results.length == 0)
-            res.status(404).send("no se encontro el empleado");
+router.delete("/:ci", function (consulta, respuesta) {
+    let criterioDeBusqueda = { ci: parseInt(consulta.params.ci) };
+    conexionABaseDeDatos.buscar(criterioDeBusqueda, coleccionEmpleados, function (error, resultados) {
+        if (error)
+            manejarError(respuesta, error.stack, 409)
+        else if (!hayResultados())
+            manejarError(respuesta, "No se encontro al empleado", 404)
         else {
-            conexionABaseDeDatos.eliminar(criterioDeBusqueda, coleccionEmpleados, function (err, resp) {
-                if (err)
-                    res.status(409).send(err.stack);
-                res.send("eliminado exitosamente");
+            conexionABaseDeDatos.eliminar(criterioDeBusqueda, coleccionEmpleados, function (error, resp) {
+                if (error)
+                    manejarError(respuesta, error.stack, 409)
+                respuesta.send("eliminado exitosamente");
             })
         }
     })
